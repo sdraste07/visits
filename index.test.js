08b4c98bb6f83fe37c4e5b3101c5db1732fbf02b
Sarthak Raste <sarthakraste@Sarthaks-iMac.local>
2020-11-01 13:07:35 +0530

    checking in entire project

---
+++ index.test.js
@@ -0,0 +1,12 @@
+describe('This is just a dummy test 1', () => {
+  it('Let test results be true === true', () => {
+    expect(true).toBe(true)
+  })
+})
+
+describe('This is just a dummy test 2', () => {
+  it('Let test results be false === false', () => {
+    expect(false).toBe(false)
+  })
+})
+
